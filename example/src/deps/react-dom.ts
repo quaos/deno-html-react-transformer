c164769de4397fae57eb1748d6a81db95d7ff3a3
Chakrit W <quaos.qrz@gmail.com>
2021-08-31 22:14:49 +0700

    Fix imports following esm.sh v34
    Fixes #3

--- example/src/deps/react-dom.ts
+++ example/src/deps/react-dom.ts
@@ -1,1 +1,2 @@
-export { default as ReactDOM } from "https://esm.sh/[react@17.0.1,react-dom@17.0.1]/react-dom?dev&no-check";
+export { default as ReactDOM } from "https://esm.sh/react-dom@17.0.2?dev&no-check";
+//"https://esm.sh/[react@17.0.1,react-dom@17.0.1]/react-dom?dev&no-check";
--- src/deps/react.ts
+++ src/deps/react.ts
@@ -1,4 +1,5 @@
-export { default as React } from "https://esm.sh/[react@17.0.1,react-dom@17.0.1]/react?dev&no-check";
+export { default as React } from "https://esm.sh/react@17.0.2?dev&no-check";
+//"https://esm.sh/[react@17.0.1,react-dom@17.0.1]/react?dev&no-check";
 
 declare global {
   namespace JSX {
